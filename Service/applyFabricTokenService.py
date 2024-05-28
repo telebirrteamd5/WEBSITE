97007feb919b72fa8216026318a2887ec05d125a
eyu-dev <169823478+eyu-dev@users.noreply.github.com>
2024-05-28 18:28:19 +0300

    Initial commit
    Signed-off-by: eyu-dev <169823478+eyu-dev@users.noreply.github.com>

---
+++ Service/applyFabricTokenService.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import urllib3
+
+urllib3.disable_warnings()
+class ApplyFabricTokenService:
+    BASE_URL = None;
+    fabricAppId = None
+    appSecret = None
+    merchantAppId = None
+    def __init__(self,BASE_URL,fabricAppId,appSecret,merchantAppId) :
+        self.BASE_URL = BASE_URL
+        self.fabricAppId = fabricAppId
+        self.appSecret = appSecret
+        self.merchantAppId = merchantAppId
+    
+    def applyFabricToken(self):
+        headers = {
+        "Content-Type":"application/json",
+        "X-APP-Key": self.fabricAppId
+        }
+        payload = {
+              "appSecret": self.appSecret
+        }
+        data=json.dumps(payload)
+        authToken = requests.post(url=self.BASE_URL,headers=headers,params=data,verify=False)
+        return authToken
